d93a6971f93a1b1078184942e818e7bcdc6c9005
david <david@DESKTOP-OHTISQ0>
2022-11-25 12:44:34 +0100

    Hecho el constructor copia

--- src/main/java/org/iesalandalus/programacion/reinaajedrez/MainApp.java
+++ src/main/java/org/iesalandalus/programacion/reinaajedrez/MainApp.java
@@ -3,6 +3,8 @@ package org.iesalandalus.programacion.reinaajedrez;
 public class MainApp {
 	
 	public static void main(String[] args) {
+		Posicion posi=new Posicion('k',20202); 
+		System.out.println(posi.getColumna());
 		
 	}
 }
