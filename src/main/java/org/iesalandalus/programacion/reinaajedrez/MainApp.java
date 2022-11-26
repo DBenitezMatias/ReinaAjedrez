cec23c4df4856f47bde39511c891fb8d2b475ac8
david <david@DESKTOP-OHTISQ0>
2022-11-26 18:23:53 +0100

    creada la clase reina y sus atributos color y posicion

--- src/main/java/org/iesalandalus/programacion/reinaajedrez/MainApp.java
+++ src/main/java/org/iesalandalus/programacion/reinaajedrez/MainApp.java
@@ -1,10 +1,12 @@
 package org.iesalandalus.programacion.reinaajedrez;
 
+import org.iesalandalus.programacion.reinaajedrez.modelo.Posicion;
+
 public class MainApp {
 	
 	public static void main(String[] args) {
 		Posicion posi=new Posicion('k',20202); 
-		System.out.println(posi.getColumna());
+		System.out.println(posi);
 		
 	}
 }
